82518c2899ef6855a882dd86c9cd68024091d021
SOUJANYASENGUPTA <96814592+SOUJANYASENGUPTA@users.noreply.github.com>
2024-02-01 22:48:06 +0530

    Update cards.jsx

--- src/components/featured/cards/cards.jsx
+++ src/components/featured/cards/cards.jsx
@@ -1,8 +1,22 @@
 import "../fpane/fpane.scss";
-
+import { motion } from "framer-motion";
+const cardVariants = {
+  initial: {
+    y: -500,
+    opacity: 0
+  },
+  animate: (custom) => ({
+    y: 0,
+    opacity: 1,
+    transition: {
+      delay: custom * 0.2, // Stagger the delay based on the index
+      duration: 2,
+    }
+  }),
+};
 const Cards=(props)=>{
    return(
-    <div class="card">
+    <motion.div class="card" variants={cardVariants} custom={props.custom}>
       <img src={props.img}/>
       <div class="content">
         <h3>{props.name}</h3>
@@ -10,7 +24,7 @@ const Cards=(props)=>{
         <a href={props.links}><button>Know more...</button></a>
       </div>
       
-    </div>
+    </motion.div>
    );
 }
 export default Cards;
