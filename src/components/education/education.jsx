200a202b71c63604322cf418cfa4498b12a58b2b
SOUJANYASENGUPTA <96814592+SOUJANYASENGUPTA@users.noreply.github.com>
2024-02-02 21:11:30 +0530

    Update education.jsx

--- src/components/education/education.jsx
+++ src/components/education/education.jsx
@@ -17,7 +17,7 @@ const history=[
 const Education=()=>{
     return(
         <div className="education">
-            <motion.div className="textContainer" initial={{opacity:0,y:400}} whileInView={{opacity:1,y:0}} transition={{delay:1,duration:1,staggerChildren:0.1}}>
+            <motion.div className="textContainer" initial={{opacity:0,y:300}} whileInView={{opacity:1,y:0}} transition={{delay:1,duration:1,staggerChildren:0.1}}>
                 <motion.h1>
                     Education
                 </motion.h1>
